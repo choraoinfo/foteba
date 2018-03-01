baf326cb39a702218a10bb73f75224d2ef401beb
Kenneth Rodolfo Becker Carletto <chorao.info@gmail.com>
2018-03-01 00:21:19 -0300

    Adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { MenuComponent } from './component/menu/menu.component';
+import { NoticiasComponent } from './component/noticias/noticias.component';
+import { routing } from './app.routing';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [    
-    AppComponent, LoginComponent, MenuComponent
+    AppComponent, LoginComponent, MenuComponent, NoticiasComponent, HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
