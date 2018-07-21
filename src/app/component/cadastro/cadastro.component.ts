79a2baa15446742ebe8f702598705a80341b2839
Kenneth Rodolfo Becker Carletto <chorao.info@gmail.com>
2018-07-21 20:56:16 -0300

    Salvar foto

--- src/app/component/cadastro/cadastro.component.ts
+++ src/app/component/cadastro/cadastro.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Atleta } from '../../entity/atleta';
 import { AtletaService } from '../../service/atleta.service';
 import { AutenticacaoService } from '../../service/autenticacao.service';
 import { ImageResolverService } from '../../service/image-resolver.service';
 import { PostService } from '../../service/post.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-cadastro',
@@ -12,25 +13,46 @@ import { PostService } from '../../service/post.service';
 })
 export class CadastroComponent implements OnInit {
 
+    @Input()
+    formulario: FormGroup;
+
     atleta: Atleta;
-    senha: string;
-    confirmar_senha: string;
-    mensagemErro: string;
-    mensagemSucesso: string;
+    salvando = false;
 
     constructor(private atletaService: AtletaService,
         private autenticacaoService: AutenticacaoService,
         private imageResolver: ImageResolverService,
-        private post: PostService) { }
+        private post: PostService,
+        private formBuilder: FormBuilder) { }
 
     ngOnInit() {
+        this.carregaAtleta();
+    }
+
+    private carregaAtleta(){
         this.atletaService.getAtleta(this.autenticacaoService.getToken()).subscribe(
             atleta => this.sucesso(atleta),
-            error => this.mensagemErro = error.message);
+            error => console.log(error));
+    }
+
+    private buildForm(){
+        this.formulario = this.formBuilder.group({
+            token: this.autenticacaoService.getToken(),
+            nome : [this.atleta.nome, Validators.required],
+            apelido : this.atleta.apelido,
+            login : { value : this.atleta.login, disabled : true },
+            ativo : { value : this.atleta.ativo, disabled : true },
+            machucado : this.atleta.machucado,
+            goleiro_fixo : this.atleta.goleiro_fixo,
+            avatar : [null],
+            senha : [null],
+            confirmar_senha : [null] 
+        });        
     }
 
     private sucesso(atleta) {
         this.atleta = atleta;
+        this.buildForm();
     }
 
     getImage() {
@@ -38,28 +60,47 @@ export class CadastroComponent implements OnInit {
             return this.imageResolver.resolveImageAvatar(this.atleta);
     }
 
-    doSubmit(form) {
-        this.mensagemErro = "";
-        this.mensagemSucesso = "";
-        if (form.valid)
-            this.post.send("atleta/cadastro", form.value)
+    doSubmit() {
+        if (this.formulario.valid)
+            this.salvando = true;
+            this.post.send("atleta/cadastro", this.formulario.value)
                 .subscribe(
                     success => this.processSuccess(success),
                     error => this.processError(error)
                 );
     }
 
-    private processSuccess(success){
-        this.senha = "";
-        this.confirmar_senha = "";
+    private processSuccess(success) {        
+        this.salvando = false;
+        this.formulario.patchValue({
+            senha : '',
+            confirmar_senha : '',
+            avatar : ''
+        });
+        this.carregaAtleta();
     }
 
-    private processError(error){
-        // this.mensagemErro = error.message;        
+    private processError(error) {
+        this.salvando = false;
     }
 
     isLogado() {
         return this.autenticacaoService.isLogged();
+    }
+
+    onFileChange(event) {
+        let reader = new FileReader();
+        if (event.target.files && event.target.files.length > 0) {
+            let file = event.target.files[0];
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                this.formulario.value.avatar = {
+                    filename: file.name,
+                    filetype: file.type,
+                    value: reader.result.split(',')[1]
+                }
+            };
+        }
     }
 
 }
