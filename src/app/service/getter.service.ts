96aef061d5c6a93df5be81498eee2d9a7008f69a
Kenneth <kenneth.becker@outlook.com>
2019-08-16 17:56:02 -0300

    Melhoria no layout

--- src/app/service/getter.service.ts
+++ src/app/service/getter.service.ts
@@ -14,7 +14,7 @@ export class GetterService {
     private message: string;
     private observableError: BehaviorSubject<string>;
 
-    BASE_URL = 'http://foteba.modafaquers.com.br/api/';
+    BASE_URL = 'http://foteba.kennethbecker.com.br/api/';
 
     constructor(private http: HttpClient,
         private autenticacaoService: AutenticacaoService) {
